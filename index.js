b2ba3eef30472792927068391f965359991a4f77
AnthonyTonev <32884987+AnthonyTonev@users.noreply.github.com>
2024-12-05 20:21:16 +0200

    Move solutions to root

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import { fez } from "fez-lisp";
 import { readFileSync, access } from "fs";
 const day = process.argv[2];
-const path = `./${day}/solution.lisp`;
+const path = `./day${day}.lisp`;
 access(path, (err) => {
   if (err) {
     console.log(`\x1b[31mSolution for day ${day} does not exist\x1b[33m\n`);
