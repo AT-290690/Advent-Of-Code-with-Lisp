ea8dacda437f3c888b174211b92222de9a2fe00b
AnthonyTonev <32884987+AnthonyTonev@users.noreply.github.com>
2024-12-08 12:42:18 +0200

    Use list for args

--- day7.lisp
+++ day7.lisp
@@ -13,20 +13,20 @@
     (let lines (|> input (string:lines) (array:map (lambda x (do 
     (let sides (|> x (string:split (array:first ":"))))
     (let L (|> sides (array:first) (from:chars->digits) (from:digits->number)))
-    (let R (|> sides (array:second) (string:words) (array:exclude array:empty?) (array:map (lambda x (|> x (from:chars->digits) (from:digits->number))))))
+    (let R (|> sides (array:second) (string:words) (array:exclude array:empty?) (from:array->list) (list:map (lambda x (|> x (from:chars->digits) (from:digits->number))))))
     '(L R)))))))))
 
 (let part1 (lambda input (do 
     (let solve (lambda args out (do 
-        (if (= (length args) 1) (= out (car args))
+        (if (list:nil? (list:tail args)) (= out (list:head args))
             (cond
-                (and (= (mod out (car args)) 0) (solve (cdr args) (/ out (car args)))) 1
-                (and (> out (car args)) (solve (cdr args) (- out (car args)))) 1
+                (and (= (mod out (list:head args)) 0) (solve (list:tail args) (/ out (list:head args)))) 1
+                (and (> out (list:head args)) (solve (list:tail args) (- out (list:head args)))) 1
                 (*) 0)))))
     (|> input 
         (array:map (lambda x (do 
             (let left (array:first x))
-            (let right (array:reverse (array:second x)))
+            (let right (list:reverse (array:second x)))
             '(left (solve right left)))))
             (array:select (lambda x (= (array:second x) 1)))
             (array:map array:first)
