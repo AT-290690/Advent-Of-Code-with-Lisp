1a03a50bce48c43790801d9eb23af35f7e8ac4b5
AnthonyTonev <32884987+AnthonyTonev@users.noreply.github.com>
2025-01-10 14:06:48 +0200

    Update lisp

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { compile, parse } from "fez-lisp";
+import { compile, evaluate, parse } from "fez-lisp";
 import { readFileSync, access } from "fs";
 const day = process.argv[2];
 const path = `./day${day}.lisp`;
@@ -7,18 +7,19 @@ access(path, (err) => {
     console.log(`\x1b[31mSolution for day ${day} does not exist\x1b[33m\n`);
   } else {
     try {
-    const solution = new Function(
-      `return ${compile(parse(readFileSync(path, "utf-8")))}`
-    )();
-    if (Array.isArray(solution))
-      console.log(
-        solution
-          .map(
-            (x, i) => `\x1b[33m${i === 0 ? " *" : "**"} \x1b[32m${x}\x1b[0m\n`
-          )
-          .join("\n")
-      );
-    } catch(err) {
+      const solution = new Function(
+        `return ${compile(parse(readFileSync(path, "utf-8")))}`
+      )();
+      // const solution = evaluate(parse(readFileSync(path, "utf-8")));
+      if (Array.isArray(solution))
+        console.log(
+          solution
+            .map(
+              (x, i) => `\x1b[33m${i === 0 ? " *" : "**"} \x1b[32m${x}\x1b[0m\n`
+            )
+            .join("\n")
+        );
+    } catch (err) {
       console.log(`\x1b[31m${err}\x1b[33m\n`);
     }
   }
